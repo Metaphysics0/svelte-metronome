6555581e74f58853553d9c3a26c1e5dff1d56f10
Ryan Roberts <ryanr@cityhive.net>
2023-08-29 22:12:03 +0300

    just need to implement ply

--- src/ui/buttons/PlayButton.svelte
+++ src/ui/buttons/PlayButton.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+	import { audioSrcMap, audioSrcPrefix } from '$lib/constants/audioSrcMap';
 	import { buttonColorMapping } from '$lib/constants/buttonColorMapping';
 	import { currentTempoStore } from '../../stores/currentTempoStore';
 	import { isPlayingStore } from '../../stores/isPlayingStore';
 	import Icon from '@iconify/svelte';
 
 	let currentColor: string = buttonColorMapping.default;
+	let currentTempo: number;
 	currentTempoStore.subscribe((v) => {
+		currentTempo = v;
 		// @ts-ignore
 		currentColor = buttonColorMapping[v];
 	});
@@ -16,7 +19,18 @@
 	});
 
 	function setIsPlaying(e: any): void {
+		isPlaying ? pauseAudio() : playAudio();
+
 		isPlayingStore.set(!isPlaying);
+	}
+
+	function playAudio() {
+		// @ts-ignore
+		console.log('need to implement play');
+	}
+
+	function pauseAudio() {
+		console.log('need to implement pause');
 	}
 </script>
 
