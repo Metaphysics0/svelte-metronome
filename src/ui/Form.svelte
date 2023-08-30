4b3450d6f0b306ee7a61dbd116a88ed18daee5b7
Ryan Roberts <ryanr@cityhive.net>
2023-08-30 09:30:43 +0300

    better ui

--- src/ui/Form.svelte
+++ src/ui/Form.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { buttonColorMapping } from '$lib/constants/buttonColorMapping';
 	import { currentTempoStore } from '../stores/currentTempoStore';
+	import Title from './Title.svelte';
 	import DecreaseTempoButton from './buttons/DecreaseTempoButton.svelte';
 	import IncreaseTempoButton from './buttons/IncreaseTempoButton.svelte';
 	import PlayButton from './buttons/PlayButton.svelte';
@@ -19,26 +20,35 @@
 	}
 </script>
 
-<section class="w-1/4 mx-auto mt-10 font-sans border border-slate-200 p-4">
-	<div class="flex justify-between mb-5">
-		<p>
-			<strong class="text-4xl">{currentTempo}</strong>
-			<span>BPM</span>
-		</p>
-		<PlayButton />
-	</div>
-	<div class="flex items-center">
-		<DecreaseTempoButton />
-		<input
-			id="default-range"
-			type="range"
-			max={MAX_VALUE}
-			min={MIN_VALUE}
-			style={`accent-color: ${buttonColorMapping[currentTempo]}`}
-			on:input={handleTempoChange}
-			bind:value={currentTempo}
-			class="w-full mx-2 outline-none h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-		/>
-		<IncreaseTempoButton />
+<section class="mx-auto mt-10 font-sans border border-slate-200 py-3">
+	<Title />
+	<div class="flex flex-col px-3 pb-3">
+		<div class="flex justify-between mb-5">
+			<p>
+				<strong class="text-4xl">{currentTempo}</strong>
+				<span>BPM</span>
+			</p>
+			<PlayButton />
+		</div>
+		<div class="flex items-center">
+			<DecreaseTempoButton />
+			<input
+				id="default-range"
+				type="range"
+				max={MAX_VALUE}
+				min={MIN_VALUE}
+				style={`accent-color: ${buttonColorMapping[currentTempo]}`}
+				on:input={handleTempoChange}
+				bind:value={currentTempo}
+				class="w-full mx-2 outline-none h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+			/>
+			<IncreaseTempoButton />
+		</div>
 	</div>
 </section>
+
+<style>
+	section {
+		max-width: calc(100% - 20rem);
+	}
+</style>
---
+++ src/ui/Title.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="flex justify-between mb-7 border-b border-slate-200 px-3 pb-2.9">
+	<h3 class="text-xl">Metronome</h3>
+	<button on:click={() => alert('you are the best')}>
+		<Icon color="#70757a" width="20" icon="material-symbols:share" />
+	</button>
+</div>
